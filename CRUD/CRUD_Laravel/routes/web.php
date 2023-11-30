1827c0d00737f11c340e27daa0dc99bf8c030136
Jndungue089 <68353090+Jndungue089@users.noreply.github.com>
2023-11-30 10:04:13 +0100

    Done
    - All CRUD functions works perfectly
    - There are coming updates for layout design of the page

--- CRUD/CRUD_Laravel/routes/web.php
+++ CRUD/CRUD_Laravel/routes/web.php
@@ -14,11 +14,12 @@ use App\Models\Candidato;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+# Página inicial
 Route::get('/', function () {
     return view('welcome');
 });
 
+# Rota para cadastrar candidato
 Route::post('/cadastrar_candidato', function(Request $informacoes){
     Candidato::create([
         'nome' =>$informacoes->nome_candidato,
@@ -28,7 +29,33 @@ Route::post('/cadastrar_candidato', function(Request $informacoes){
     echo "<a href='\' style='text-decoration: none;'>Voltar à página inicial</a>";
 });
 
+# Rota para mostrar o candidato
 Route::get('/mostrar-candidato/{id_do_candidato?}', function($id_do_candidato){
-    Candidato::findOrFail($id_do_candidato);
+    $candidato = Candidato::findOrFail($id_do_candidato);
+    echo $candidato->nome;# Mostra o nome do candidato
+    echo "<br>";
+    echo $candidato->telefone;# Mostra o nome do telefone
+});
+
+# Rota para editar candidato
+Route::get('/editar-candidato/{id_do_candidato?}', function($id_do_candidato){
+    $candidato = Candidato::findOrFail($id_do_candidato);
+    return view('editar_candidato', ['candidato'=> $candidato]);# Retorna a página para editar candidato
+});
+
+#nRota para atualizar candidato
+Route::put('/atualizar-candidato/{id_do_candidato}', function(Request $informacoes, $id_do_candidato){
+    $candidato =Candidato::findOrFail($id_do_candidato);
+    $candidato->nome = $informacoes->nome_candidato;
+    $candidato->telefone = $informacoes->telefone_candidato;
+    $candidato->save();# Função para atualizar candidato reescrevendo
+    echo "Candidato atualizado com sucesso!";
+});
+
+#Rota para excluir candidato
+Route::get('/excluir-candidato/{id_do_candidato?}', function($id_do_candidato){
+    $candidato = Candidato::findOrFail($id_do_candidato); 
+    $candidato->delete(); #Função para deletar candidato
+    echo "Candidato excluído com sucesso!"; 
 });
 
