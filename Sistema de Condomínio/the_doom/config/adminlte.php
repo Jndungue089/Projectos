355ad01db7c10fd1b2b350867f5b0ea8bb1137b6
Jndungue089 <68353090+Jndungue089@users.noreply.github.com>
2024-01-25 14:44:14 +0100

    Update adminlte.php

--- Sistema de Condomínio/the_doom/config/adminlte.php
+++ Sistema de Condomínio/the_doom/config/adminlte.php
@@ -192,7 +192,7 @@ return [
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
-    'classes_sidebar' => 'sidebar-dark-primary elevation-4',
+    'classes_sidebar' => 'sidebar-light-primary accent elevation-4',
     'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-white navbar-light',
     'classes_topnav_nav' => 'navbar-expand',
@@ -313,8 +313,8 @@ return [
             'url'  => 'profile',
             'icon' => 'fas fa-fw fa-user',
         ],
-        ['header'=>'MANAGE'],
-        
+        ['header' => 'MANAGE'],
+
         [
             'text'    => 'multilevel',
             'icon'    => 'fas fa-fw fa-share',
